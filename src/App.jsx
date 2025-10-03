473f63c2da5ad0a2be97c0f2e1532f0662375190
Alamin-Rintu <alaminrintu01@gmail.com>
2025-10-03 10:37:21 +0600

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         </div>
       </div>
 
-      {/* Banner section */}
+      {/* banner section */}
       <section className="w-11/12 mx-auto flex  gap-4 h-[300px] mt-5">
         <div className="w-1/2 bg-gradient-to-r from-[#632EE3] to-[#9F62F2] rounded-lg flex flex-col justify-center items-center text-white relative overflow-hidden">
           <img
@@ -57,6 +57,60 @@ function App() {
           <p className="text-3xl font-bold">0</p>
         </div>
       </section>
+
+      {/* footer section */}
+
+      <footer className="bg-neutral text-neutral-content p-10 mt-5">
+        <div className="flex flex-col sm:flex-row gap-10 justify-center ">
+          <nav className="flex-1">
+            <h6 className="footer-title text-2xl">CS-Ticket System</h6>
+            <p className="mt-2">
+              Lorem Ipsum is simply dummy text of the <br />
+              printing and typesetting industry. Lorem <br />
+              Ipsum has been the industry's standard <br />
+              dummy text ever since the 1500s, when an <br />
+              unknown printer took a galley of type and <br />
+              scrambled it to make a type specimen book.
+            </p>
+          </nav>
+
+          <nav className="flex-1">
+            <h6 className="footer-title">Company</h6>
+            <a className="link link-hover">About us</a> <br />
+            <a className="link link-hover">Our Mission</a> <br />
+            <a className="link link-hover">Contact Sales</a>
+          </nav>
+
+          <nav className="flex-1">
+            <h6 className="footer-title">Services</h6>
+            <a className="link link-hover">Product & Services</a> <br />
+            <a className="link link-hover">Customer Stories</a> <br />
+            <a className="link link-hover">Download Apps</a>
+          </nav>
+
+          <nav className="flex-1">
+            <h6 className="footer-title">Information</h6>
+            <a className="link link-hover">Privacy Policy</a> <br />
+            <a className="link link-hover">Terms & Conditions</a> <br />
+            <a className="link link-hover">Join Us</a>
+          </nav>
+
+          <nav className="flex-1">
+            <h6 className="footer-title">Social Links</h6> <br />
+            <a className="link link-hover">@CS-Ticket System</a><br />
+            <a className="link link-hover">@CS-Ticket System</a><br />
+            <a className="link link-hover">@CS-Ticket System</a><br />
+            <a className="link link-hover">support@cst.com</a>
+          </nav>
+        </div>
+
+        <p className="text-center mt-10 border-t border-neutral/50 pt-5">
+          Copyright © {new Date().getFullYear()} - All rights reserved
+        </p>
+      </footer>
+
+
+
     </>
   )
 }
